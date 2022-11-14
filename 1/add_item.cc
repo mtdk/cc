a3ba7f912a25f097d45e9f8476b9e04564788ddd
mtdk <ruixiangxie@outlook.com>
2022-11-14 21:49:23 +0800

    202211142149

---
+++ 1/add_item.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Sales_itme.h"
+int main()
+{
+    Sales_item item1, item2;
+
+    std::cin >> item1 >> item2;
+    std::cout << item1 + item2 << std::endl;
+
+    return 0;
+}
