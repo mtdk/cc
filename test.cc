8993047e770bb88b2624bb69d0d817068ddce050
QINGHE-XRX <ruixiangxie@hotmail.com>
2022-11-15 17:22:39 +0800

    这是一个测试文件

---
+++ test.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+
+std::string global_str;
+int global_int;
+
+int main(){
+
+    // std::cin >> int input_value >> std::endl;
+
+    // std::cout << input_value << std::endl;
+
+    // int i = {3.14};
+
+    // double salary = wage = 9999.99;
+
+    // int i = 3.14;
+
+    int local_int;
+    std::string local_str;
+
+    return 0;
+}
