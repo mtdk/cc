5ac106bfc7ec967210f8a1489ce43f44dbba600a
QINGHE-XRX <ruixiangxie@hotmail.com>
2022-11-22 11:54:29 +0800

    202211221154

--- test.cc
+++ test.cc
@@ -1,22 +1,15 @@
-#include<iostream>
+#include <iostream>
 
-std::string global_str;
-int global_int;
+// std::string global_str;
+// int global_int;
+int i = 42;
 
-int main(){
 
-    // std::cin >> int input_value >> std::endl;
-
-    // std::cout << input_value << std::endl;
-
-    // int i = {3.14};
-
-    // double salary = wage = 9999.99;
-
-    // int i = 3.14;
-
-    int local_int;
-    std::string local_str;
-
+int main()
+{
+    int i, &ri = i;
+    i = 5;
+    ri = 10;
+    std::cout << i << " " << ri << std::endl;
     return 0;
 }
